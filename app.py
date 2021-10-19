0eb75f689de45f6406e6a89f827f71179274c3e0
Viski Paul <58706499+viskipaul@users.noreply.github.com>
2021-10-20 01:05:01 +0300

    Added create table method

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def main_controller():
     elif re.search('(use database )[a-zA-Z]+', command):
         database_name = command.split(' ')[2]
         return use_database(database_name)
+    elif re.search('(drop database )[a-zA-Z]+', command):
+        database_name = command.split(' ')[2]
+        return drop_database(database_name)
+    elif re.search('(create table )[a-zA-Z()0-9]*', command):
+        return create_table(command)
 
 
 def create_database(name) -> str:
@@ -34,6 +39,31 @@ def use_database(name) -> str:
     f.write(name)
     f.close()
     return 'Using database: ' + name + '.'
+
+
+def drop_database(name) -> str:
+    xml = ET.parse('database.xml')
+    for elem in xml.iter():
+        if elem.attrib.get('name') == name:
+            xml.getroot().remove(elem)
+    xml.write('database.xml')
+    return 'Succesfully dropped database ' + name + '.'
+
+
+def create_table(command: str) -> str:
+    table_name, columns_dict = validate_create_command(command)
+    xml = ET.parse('database.xml')
+
+
+
+def validate_create_command(command: str):
+    table_name = command.split(' ')[2]
+    columns = command.split(' ( ')[1]
+    columns_list = columns.split(',')
+    columns_dict = {}
+    for column in columns_list:
+        columns_dict[column.strip().split(' ')[0]] = column.strip().split(' ')[1]
+    return table_name, columns_dict
 
 
 if __name__ == '__main__':
